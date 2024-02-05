9f5632278a1dd97ca617ab6cef837d92a8389681
André de Lucca Venturini <155122346+venturiniandre@users.noreply.github.com>
2024-02-05 00:11:38 -0300

    Update Estacionamento.cs

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -16,29 +16,24 @@ namespace DesafioFundamentos.Models
         {
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             string placa = Console.ReadLine();
-            veiculos.Add(placa);
+            veiculos.Add(placa
+            Console.WriteLine("Veículo estacionado!");
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-           
-
-            string placa = Console.ReadLine();
+           string placa = Console.ReadLine();
 
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-           
-
                 int horas = int.Parse(Console.ReadLine());
-                decimal valorTotal = (horas * this.precoPorHora) + this.precoInicial; 
-
+                decimal valorTotal = precoInicial + precoPorHora * horas;
           
                 veiculos.Remove(placa);
-
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
